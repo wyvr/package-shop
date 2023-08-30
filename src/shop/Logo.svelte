4cb07a584918f350ce205e2428c43f24607656dd
wyvr <wyvr@daviot.at>
2023-08-30 16:15:31 +0200

    :sparkles: add name and url to logo

--- src/shop/Logo.svelte
+++ src/shop/Logo.svelte
@@ -1,1 +1,17 @@
-<img src="/assets/logo.svg" alt="" width="150" height="50" />
+<script>
+    import { Config } from '@wyvr/generator/src/utils/config.js';
+    import { url_join } from '@src/shop/core/url.mjs';
+
+    export let name = '';
+    export let store = 'de';
+
+    $: url = url_join(store);
+
+    onServer(() => {
+        if (!name) {
+            name = Config.get('shop.name');
+        }
+    });
+</script>
+
+<a href={url}><img src="/assets/logo.svg" alt={name} width="150" height="50" /></a>
--- wyvr.js
+++ wyvr.js
@@ -1,8 +1,11 @@
 export default {
     shop: {
+        name: 'wyvr Shop',
+        domain: 'your.domain.shop',
         default_store: null,
         stores: {},
     },
+
     _secrets: {
         shop: {
             customer_email: '',
