1a1958ac064403358dc5a962ed91c2eeb8ea905b
wyvr <wyvr@daviot.at>
2023-09-04 17:21:27 +0200

    :bug: fix stock for configurable products

--- src/shop/core/product/get_stock.mjs
+++ src/shop/core/product/get_stock.mjs
@@ -3,7 +3,9 @@ import { get_attribute_value } from '../attributes.mjs';
 export function get_stock(product) {
     const stock = get_attribute_value(product, 'stock');
     if (product?.type_id == 'configurable' && product?.configurable_products) {
-        const child_stocks = product.configurable_products.map((child) => get_attribute_value(child, 'stock'));
+        const child_stocks = product.configurable_products
+            .map((child) => get_attribute_value(child, 'stock'))
+            .filter(Boolean);
         stock.is_in_stock = child_stocks.find((s) => s.is_in_stock == '1') != null;
         stock.qty = Math.max(...child_stocks.map((s) => parseFloat(s.qty)));
     }
