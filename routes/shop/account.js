acdba372810e5f35829825a93e9d19598a60b60a
wyvr <wyvr@daviot.at>
2023-09-05 11:46:18 +0200

    :bug: remove unneeded onExec

--- routes/shop/account.js
+++ routes/shop/account.js
@@ -2,17 +2,6 @@ import { Config } from '@wyvr/generator/src/utils/config.js';
 
 export default {
     url: '/[store]/account',
-    onExec: async ({ params }) => {
-        const stores = Config.get('shop.stores');
-        const store_id = stores[params.store];
-        return {
-            store: {
-                key: params.store,
-                value: store_id,
-            },
-            stores,
-        };
-    },
     _wyvr: ({ data }) => {
         return {
             template: ['shop/Account', 'shop/Default'],
--- routes/shop/product-to-cart.js
+++ routes/shop/product-to-cart.js
@@ -2,17 +2,6 @@ import { Config } from '@wyvr/generator/src/utils/config.js';
 
 export default {
     url: '/[store]/product-to-cart',
-    onExec: async ({ params }) => {
-        const stores = Config.get('shop.stores');
-        const store_id = stores[params.store];
-        return {
-            store: {
-                key: params.store,
-                value: store_id,
-            },
-            stores,
-        };
-    },
     _wyvr: ({ data }) => {
         return {
             template: ['shop/ProductToCart', 'shop/Default'],
