f06bed00d55e247d9bae5d1a6c0752737735f67a
wyvr <wyvr@daviot.at>
2023-09-06 17:35:35 +0200

    :lipstick: remove empty tag and use pseudo element

--- src/shop/component/Stock.svelte
+++ src/shop/component/Stock.svelte
@@ -8,23 +8,19 @@
 {#if is_in_stock}
     {#if qty < 5}
         <div class="in_stock red">
-            <span class="indicator" />
             <span class="text"> {__('product_stock.only_x_in_stock', { qty })}</span>
         </div>
     {:else if qty < 10}
         <div class="in_stock yellow">
-            <span class="indicator" />
             <span class="text in_stock"> {__('product_stock.only_x_in_stock', { qty })}</span>
         </div>
     {:else}
         <div class="in_stock green">
-            <span class="indicator" />
             <span class="text"> {__('product_stock.in_stock', { qty })}</span>
         </div>
     {/if}
 {:else}
     <div class="out_of_stock">
-        <span class="indicator" />
         <span class="text">{__('product_stock.out_of_stock', { qty })} </span>
     </div>
 {/if}
