b4045c43096bd2a60bee3aa4a575fc0ef64ebe45
wyvr <wyvr@daviot.at>
2023-09-28 10:10:55 +0200

    :sparkles: logout the customer in case of forbidden error

--- src/shop/api-client/cart/load_cart.js
+++ src/shop/api-client/cart/load_cart.js
@@ -2,6 +2,7 @@ import { get_time_stamp_minutes } from '@src/shop/core/cache_breaker.mjs';
 import { get_domain } from '@src/shop/api-client/get_domain';
 import { get_store } from '@src/shop/api-client/get_store';
 import { url_join } from '@src/shop/core/url.mjs';
+import { customer_logout } from '../../logic/customer_logout';
 
 export async function load_cart(email_or_token, bearer_token, domain_url, store_key) {
     store_key = get_store(store_key);
@@ -18,6 +19,10 @@ export async function load_cart(email_or_token, bearer_token, domain_url, store_
         }
         const url = url_join(domain_url, store_key, 'api', 'cart', email_or_token || 'guest');
         const response = await fetch(url + `?cb=${cb}`, data);
+        if (!response.ok && response.status == 403) {
+            customer_logout();
+            return [undefined, undefined];
+        }
         cart = await response.json();
     } catch (e) {
         console.error(e);
