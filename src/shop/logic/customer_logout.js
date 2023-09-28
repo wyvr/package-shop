a2f9c229f4e4b70c6a59ad9ca01dea458aa92a73
wyvr <wyvr@daviot.at>
2023-09-28 10:10:02 +0200

    :bug: destroy guest cart on logout

--- src/shop/logic/customer_logout.js
+++ src/shop/logic/customer_logout.js
@@ -1,8 +1,11 @@
 import { token } from '@src/shop/stores/token';
 import { customer } from '@src/shop/stores/customer';
 import { messages } from '@src/shop/stores/messages';
+import { guest_cart_name } from '@src/shop/stores/cart';
+import { save } from '@src/shop/stores/storage';
 
 export function customer_logout() {
+    save(guest_cart_name, undefined);
     token.set(undefined);
     customer.set(undefined);
     messages.push(__('customer.logout_success'), 'success');
