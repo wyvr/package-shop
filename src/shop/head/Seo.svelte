1cb222c0bd97105da5f17fba84279e86a5ce0142
wyvr <wyvr@daviot.at>
2023-10-17 12:41:06 +0200

    :sparkles: add basic seo

--- src/shop/head/Seo.svelte
+++ src/shop/head/Seo.svelte
@@ -1,7 +1,12 @@
 <script>
-    export let data;
+    import { url_join } from '@src/shop/core/url.mjs';
 
-    $: title = data.meta?.title || data.title || _inject('config.shop.name');
+    export let data;
+    const name = injectConfig('shop.name');
+    $: title = data.meta?.title || data.meta_title || data.title || name;
+    $: description = data.meta?.description || data.meta_description || name;
+    $: canonical = url_join(domain, data.url);
+    $: robots = data?.robots || injectConfig('env', 'dev') == 'prod' ? 'index, follow' : 'noindex, nofollow';
 </script>
 
 <title>{title}</title>
@@ -10,8 +15,17 @@
     <meta name="og:title" content={title} />
 {/if}
 
-{#if data.meta && data.meta.description}
-    <meta content={data.meta.description} name="description" />
+{#if description}
+    <meta name="description" content={description} />
+    <meta name="og:description" content={description} />
+{/if}
+
+{#if canonical}
+    <link rel="canonical" href={canonical} />
+{/if}
+
+{#if robots}
+    <meta name="robots" content={robots} />
 {/if}
 
 <meta charset="utf-8" />
