c69e55fec7a43997c515097f4f8c81693444dc5a
wyvr <wyvr@daviot.at>
2023-11-22 11:05:06 +0100

    :sparkles: add unknown api route

---
+++ routes/shop/api/unknown.js
@@ -0,0 +1,14 @@
+import { sleep_random } from '@src/shop/api/sleep.js';
+
+export default {
+    url: '/[store]/api/.*',
+    onExec: async ({ returnJSON }) => {
+        await sleep_random(200, 500);
+        return returnJSON(
+            {
+                message: 'unknown',
+            },
+            404
+        );
+    },
+};
