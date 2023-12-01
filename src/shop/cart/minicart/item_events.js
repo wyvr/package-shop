88ab71c57c19a457dd19864d2cfd84decb975d95
wyvr <wyvr@daviot.at>
2023-12-01 10:42:16 +0100

    :fire: remove cache from items events

--- src/shop/cart/minicart/item_events.js
+++ src/shop/cart/minicart/item_events.js
@@ -1,15 +1,8 @@
 import { cart } from '@src/shop/stores/cart';
 
 let debouncer;
-const cache = {};
 
 export function update_qty_delayed(sku, qty, on_started, on_finished) {
-    if (cache[sku] == null || cache[sku] != qty) {
-        cache[sku] = qty;
-    } else {
-        // when nothing change avoid updating
-        return;
-    }
     // delay the cart update, to avoid bruteforcing the api
     if (debouncer) {
         clearTimeout(debouncer);
@@ -27,8 +20,6 @@ export function update_qty_delayed(sku, qty, on_started, on_finished) {
 
 export async function update_qty(sku, qty) {
     const new_qty = qty || 0;
-    if (new_qty) {
-        delete cache[sku];
-    }
+
     return await cart.update_item(sku, new_qty);
 }
