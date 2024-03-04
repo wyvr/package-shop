4b84c7a308bbbd2743ccfb24f4e465d69e221cb0
wyvr <info@wyvr.dev>
2024-03-04 12:05:04 +0100

    bug: add null check to the currency formating

--- src/shop/core/currency.mjs
+++ src/shop/core/currency.mjs
@@ -3,6 +3,13 @@ export function to_currency(price, locale = 'en-UK', currency = 'EUR') {
     if (updated_price == null) {
         updated_price = 0;
     }
+    // add null check because null is a defined value
+    if(!locale) {
+        locale = 'en-UK';
+    }
+    if(!currency) {
+        currency = 'EUR';
+    }
     // @see https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
     const format = new Intl.NumberFormat(locale, {
         minimumFractionDigits: 2,
