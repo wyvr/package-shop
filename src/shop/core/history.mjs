ad7ece0e76576887322885b1c9b2f54af1e436ee
wyvr <info@wyvr.dev>
2024-03-26 10:08:01 +0100

    feat: add new replace_history method

--- src/shop/core/history.mjs
+++ src/shop/core/history.mjs
@@ -22,6 +22,24 @@ export function add_history(data = null, title = document.title, url = location.
     history.pushState(data, title, url);
 }
 
+/**
+ * Function to replace the current history state. Uses the History API's replaceState method to
+ * replace the current history entry.
+ *
+ * @param {*} data The state object is a JavaScript object which is associated with the new history entry created by replaceState(). This can be null.
+ * @param {string} title The title for the new history entry. This can be null.
+ * @param {string} url The URL for the new history entry.
+ * @returns {void}
+ */
+export function replace_history(data = null, title = document.title, url = location.pathname) {
+    if (last_url === url) {
+        return;
+    }
+    last_url = url;
+    history.replaceState(data, title, url);
+}
+
+// global event handler
 if (isClient) {
     window.addEventListener('popstate', (event) => {
         trigger('history', event);
